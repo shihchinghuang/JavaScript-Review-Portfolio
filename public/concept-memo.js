76b829db0bf8175fa81aff9ecbbf81e0b8e511d3
shihchinghuang <82016503+shihchinghuang@users.noreply.github.com>
2021-08-16 12:16:14 +0800

    日曆 debug、選單控制

--- public/concept-memo.js
+++ public/concept-memo.js
@@ -1,3 +1,6 @@
+// JavaScript 陣列處理方法 [filter(), find(), forEach(), map(), every(), some(), reduce()] https://wcc723.github.io/javascript/2017/06/29/es6-native-array/
+
+// ------------------for / filter------------------
 const aArray = [1, 2, 3, 4, 5]
 
 // 透過for 回傳沒有3的陣列
@@ -17,3 +20,18 @@ const cArray = aArray.filter(function (value) {
 
 //filter 改成箭頭函式，箭頭函式不用function也不用return
 const dArray = aArray.filter((value) => value !== 3)
+// ------------------for / filter------------------
+
+// ------------------for迴圈 / map------------------
+// for 迴圈的寫法
+let weekDayDisplay = ''
+
+for (let i = 0; i < weekList.length; i++) {
+  weekDayDisplay += `<th> ${weekList[i]} </th>`
+}
+title.innerHTML = `<tr> ${weekDayDisplay} </tr>`
+
+// map + 箭頭函式的寫法（箭頭函式不用return）
+const weekListMap = weekList.map((v) => `<th> ${v} </th>`).join('')
+title.innerHTML = `<tr> ${weekListMap} </tr>`
+// ------------------for迴圈 / map------------------
--- public/one-page-features-collection/index.js
+++ public/one-page-features-collection/index.js
@@ -384,7 +384,7 @@ const data = document.querySelector('#data')
 // new Date() 為產生物件的本地日期與時間
 const now = new Date()
 
-// 要得到今天的西元年使用Date物件的getFullYear()，要得到月份使用getMonth()(注意回傳為 0~11)，要得到日期使用getDate()(注意回傳值為 1-31)
+// 要得到今天的西元年使用Date物件的getFullYear()，要得到月份使用getMonth()(注意回傳為 0~11)
 const nowY = now.getFullYear()
 const nowM = now.getMonth() + 1 // 回傳為0-11，所以要+1
 
@@ -400,27 +400,28 @@ const weekList = [
   '星期六',
 ]
 
+// for 迴圈的寫法
 let weekDayDisplay = ''
 
-// for 迴圈的寫法
 for (let i = 0; i < weekList.length; i++) {
   weekDayDisplay += `<th class="border border-dark"> ${weekList[i]} </th>`
 }
 title.innerHTML = `<tr class="border border-dark"> ${weekDayDisplay} </tr>`
 
-// map + 箭頭函式的寫法
-// const weekListMap = weekList
-//   .map((v) => `<span class="m-1 border border-dark"> ${v} </span>`)
-//   .join('')
-// title.innerHTML = weekListMap
+// map + 箭頭函式的寫法（箭頭函式不用return）
+const weekListMap = weekList
+  .map((v) => `<th class="border border-dark"> ${v} </th>`)
+  .join('')
+title.innerHTML = `<tr class="border border-dark"> ${weekListMap} </tr>`
 
 // 要得到某年的某個月有幾天，可以用new Date(y, m, 0).getDate()，例如 2009 年的 9 月就是使用new Date(2009, 9, 0).getDate()
+// 注意，是指上個月的最後一天是幾號
 const days = new Date(nowY, nowM, 0).getDate()
 
-// getDay() 括號中寫日期，可以得到 0-6 的值，代表是星期幾，其中 0 代表星期日
-const firstDay = new Date(nowY, nowM - 1, 14).getDay()
+// 這個月的第一天是星期幾，getDay() 括號中寫日期，可以得到 0-6 的值，代表是星期幾，其中 0 代表星期日。最後一個 1 代表1 號。
+const firstDay = new Date(nowY, nowM - 1, 1).getDay()
 
-// 日曆先用空陣列
+// 本月所有日期先用空陣列
 const daysDataArray = []
 
 // 補前面的空白
@@ -428,7 +429,7 @@ for (let i = 0; i < firstDay; i++) {
   daysDataArray.push('')
 }
 
-// 後面用 1~31 補，如果沒有 i+1 ，會變成 0~30
+// 加入本月所有的日期資料，用 1~31 補，如果沒有 i+1 ，會變成 0~30
 for (let i = 0; i < days; i++) {
   daysDataArray.push(i + 1)
 }
@@ -448,3 +449,43 @@ for (let i = 0; i < daysDataArray.length; i++) {
 data.innerHTML = `<tr class="border border-dark">${daysDisplay}</tr>`
 
 // ------------------日曆------------------
+
+// ------------------選單控制------------------
+// dataSet可以輔助使用
+
+// 注意 js 命名不能用「-」，要選一個的話用 querySelector，選多個用 querySelectorAll，會是一個陣列。
+const menuItems = document.querySelectorAll('.menu-item')
+
+// addEventListener 只能一個一個加，不能一次加多個，所以要用 for 迴圈
+for (let i = 0; i < menuItems.length; i++) {
+  //js 滑鼠事件 https://www.w3schools.com/js/js_htmldom_events.asp
+  // 常見的滑鼠事件包含 mouseover / mouseout，相當於 css 裡的 hover
+
+  menuItems[i].addEventListener('mouseenter', function (event) {
+    // classList https://developer.mozilla.org/zh-TW/docs/Web/API/Element/classList
+    // classList.add 代表多加了一個class名稱（記得在 css 要設定該 class 的樣式）
+    this.classList.add('enter')
+
+    console.log(event.target.innerText)
+    console.log(this.innerText)
+    // event.target = this，用 this 就可以不用加參數
+    // 加掛 innerText 可以獲得裡面的字
+    // mouseover 印出的是 <a href="">關於我們</a>
+    // mouseenter 印出的是 <li class="menu-item"><a href="">關於我們</a></li>
+
+    // 嘗試用 if 來加icon
+    // if (this.innerText === '首頁') {
+    //   this.addEventListener('mouseenter', function () {
+    //     this.innerHTML = `<a href=""><i class="fa fa-home" aria-hidden="true"></i>首頁</a>`
+    //   })
+    //   this.addEventListener('mouseleave', function () {
+    //     this.innerHTML = `<a href="">首頁</a>`
+    //   })
+    // }
+  })
+
+  menuItems[i].addEventListener('mouseleave', function (event) {
+    event.target.classList.remove('enter')
+  })
+}
+// ------------------選單控制------------------
