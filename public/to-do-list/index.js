0d9cadc6a2e5d750d4b595778da9a599b7fd45c1
shihchinghuang <82016503+shihchinghuang@users.noreply.github.com>
2021-08-23 17:31:18 +0800

    to-do-list 完成新增刪除勾選及 localstorage 新增刪除

--- public/to-do-list/index.js
+++ public/to-do-list/index.js
@@ -1,81 +1,231 @@
-const add = document.querySelector('#add')
-const input = document.querySelector('#input')
-const item = document.querySelector('#item')
-const check = document.querySelector('.check-icon')
+// 0823 待辦
+// select 要選兩次才會觸發篩選
+// select 的排版，或是換成按鈕看看
+// 研究編輯，或把編輯 icon 拿掉
+// sweet alert 微調
 
-add.addEventListener('click', function () {
-  const itemList = []
+const add = document.querySelector("#add");
+const input = document.querySelector("#input");
+const todo = document.querySelector("#todo");
+const filter = document.querySelector(".filter");
+
+document.addEventListener("DOMContentLoaded", getLocalTodos);
+// 頁面 load 進來就會取得上次存在 localstorage 的資料
+
+add.addEventListener("click", addTodo);
+todo.addEventListener("click", deleteCheckTodo);
+filter.addEventListener("click", filterTodo);
+
+//-----新增待辦事項-----
+function addTodo(e) {
+  // 防止表單被送出
+  e.preventDefault();
 
   if (input.value.trim() == 0) {
     // 如果輸入資料是空格或沒有輸入
 
-    swal('請輸入內容', '', 'warning') // sweet alert
+    swal("請輸入內容", "", "warning"); // sweet alert
   } else {
-    saveTodo(input.value) //把資料存進localstorage
+    // 製造以下東西
+    // <div class="item">
+    //   <li></li>
+    //   <i class="check-icon far fa-check-circle"></i>
+    //   <i class="edit-icon far fa-edit"></i>
+    //   <i class="trash-icon far fa-trash-alt"></i>
+    // </div>
 
-    itemList.unshift(input.value) // 增加輸入的東西到陣列，用unshift讓新輸入的在最上面，push會讓新輸入的在最下面
-    console.log(itemList)
+    // 製造 <div class="item"></div>
+    const item = document.createElement("div");
+    item.classList.add("item", "mb-2");
 
-    for (let i = 0; i < itemList.length; i++) {
-      item.innerHTML += `<li class="d-flex justify-content-between"> ${itemList[i]}
-            <span class="check-icon">
-                <i class="far fa-check-circle"></i>
-            </span>
-            <span class="edit-icon">
-                <i class="far fa-edit"></i>
-            </span>
-            <span class="trash-icon">
-                <i class="far fa-trash-alt"></i>
-            </span>
-        </li>`
+    // 製造 <li></li>
+    const newTodo = document.createElement("li");
+    newTodo.innerText = `${input.value}`;
+    item.appendChild(newTodo);
+
+    // 製造打勾
+    const checkIcon = document.createElement("i");
+    checkIcon.innerHTML = "";
+    checkIcon.classList.add("check-icon", "far", "fa-check-circle");
+    newTodo.appendChild(checkIcon);
+
+    // 製造編輯
+    const editIcon = document.createElement("i");
+    editIcon.innerHTML = "";
+    editIcon.classList.add("edit-icon", "far", "fa-edit");
+    newTodo.appendChild(editIcon);
+
+    // 製造刪除
+    const trashIcon = document.createElement("i");
+    trashIcon.innerHTML = "";
+    trashIcon.classList.add("trash-icon", "far", "fa-trash-alt");
+    newTodo.appendChild(trashIcon);
+
+    // console.log(item)
+    // 整個完成
+    todo.appendChild(item);
+
+    // 輸入之後存進 localstorage
+    saveLocalTodos(input.value);
+    // 清空輸入框
+    input.value = "";
+  }
+}
+
+//-----刪除 / 勾選完成的待辦事項-----
+function deleteCheckTodo(e) {
+  // 先測一下按到哪裡
+  // console.log(e.target.parentElement.parentElement)
+  const click = e.target;
+
+  //-----刪除待辦事項-----
+  // 第0個class是trash-icon的話
+  if (click.classList[0] === "trash-icon") {
+    const todoItem = click.parentElement.parentElement;
+
+    // 做刪除的動畫（但是是 transform 不是 animation）
+    todoItem.classList.add("fall");
+
+    // todoItem.remove() 用remove無法搭配動畫
+
+    // 加掛事件監聽器在 transition結束之後，所以是transition「end」觸發函式
+    todoItem.addEventListener("transitionend", function () {
+      // console.log('deleted')
+      todoItem.remove();
+    });
+
+    // 按刪除後從 localstorage 也刪除
+    removeLocalTodos(todo);
+  }
+
+  //-----完成待辦事項後勾選-----
+  if (click.classList[0] === "check-icon") {
+    const todoItem = click.parentElement.parentElement;
+    todoItem.classList.toggle("checked");
+  }
+}
+
+//-----篩選已完成 / 未完成的待辦事項-----
+function filterTodo(e) {
+  const todos = todo.childNodes;
+  console.log("select");
+
+  todos.forEach(function (t) {
+    // value 指的是 html 中 <option value="all">All</option> 的 value
+    switch (e.target.value) {
+      case "all":
+        t.style.display = "flex";
+        break;
+      case "completed":
+        if (t.classList.contains("checked")) {
+          t.style.display = "flex";
+          console.log("check");
+        } else {
+          t.style.display = "none";
+        }
+        break;
+      case "uncompleted":
+        if (!t.classList.contains("checked")) {
+          t.style.display = "flex";
+          console.log("not");
+        } else {
+          t.style.display = "none";
+        }
+        break;
     }
+  });
+}
 
-    input.value = '' // 送出之後，清空輸入欄
-  }
-})
-
-// localstorage 存資料
-function saveTodo(todo) {
-  let todos = []
-
-  if (localStorage.getItem('todos') == null) {
-    // 如果localtorage裡面沒有todos這個陣列，todos名字是隨便取的
-    todos = [] // 就建立一個空陣列
+//-----將資料存進 localstorage-----
+function saveLocalTodos(t) {
+  // 檢查localtorage裡面有沒有todos，todos名字是隨便取的
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = []; // 沒有的話就給空陣列
   } else {
-    // 如果已經有了
-
-    todos = JSON.parse(localStorage.getItem('todos')) // 就把拿到的東西用localstorage看得懂的方式存進去 JSON.parse()把json字串變成js的數值
+    todos = JSON.parse(localStorage.getItem("todos"));
+    // 有的話就把拿到的陣列從 localstorage 取出，JSON.parse() 把json 字串變成 js 的數值
   }
-
-  todos.unshift(todo) // 加入新的資料
-  localStorage.setItem('todos', JSON.stringify(todos)) // 把東西存進localstorage JSON.stringify()是將js數值變json字串
+  todos.push(t); // 加入新的資料（新的在最後面，如果新的要在最前面，用push）
+  localStorage.setItem("todos", JSON.stringify(todos));
+  // 把東西存進 localstorage，JSON.stringify() 是將 js 數值變 json 字串
 }
 
-// localstorage 取資料
-function viewTodo(todo) {
-  if (localStorage.getItem('todos') !== null) {
-    // 如果localtorage裡面沒有todos這個陣列
-
-    item.innerHTML = JSON.parse(localStorage.getItem('todos'))
+//-----從 localstorage 取出資料-----
+function getLocalTodos() {
+  // 檢查localtorage裡面有沒有todos，todos名字是隨便取的
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = []; // 沒有的話就給空陣列
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+    // 有的話就把拿到的陣列從 localstorage 取出，JSON.parse() 把json 字串變成 js 的數值
   }
+
+  todos.forEach(function (t) {
+    // ---把上面新增待辦事項的部分複製過來---
+
+    // 製造 <div class="item"></div>
+    const item = document.createElement("div");
+    item.classList.add("item", "mb-2");
+
+    // 製造 <li></li>
+    const newTodo = document.createElement("li");
+    // newTodo.innerText = `${input.value}` 這行不用了，改成下一行
+    newTodo.innerText = t;
+    item.appendChild(newTodo);
+
+    // 製造打勾
+    const checkIcon = document.createElement("i");
+    checkIcon.innerHTML = "";
+    checkIcon.classList.add("check-icon", "far", "fa-check-circle");
+    newTodo.appendChild(checkIcon);
+
+    // 製造編輯
+    const editIcon = document.createElement("i");
+    editIcon.innerHTML = "";
+    editIcon.classList.add("edit-icon", "far", "fa-edit");
+    newTodo.appendChild(editIcon);
+
+    // 製造刪除
+    const trashIcon = document.createElement("i");
+    trashIcon.innerHTML = "";
+    trashIcon.classList.add("trash-icon", "far", "fa-trash-alt");
+    newTodo.appendChild(trashIcon);
+
+    // console.log(item)
+    // 整個完成
+    todo.appendChild(item);
+
+    // 輸入之後存進 localstorage
+    // saveLocalTodos(input.value)
+  });
 }
 
-// check.addEventListener('click', function () {
-//   alert('d')
-// })
+//-----從 localstorage 刪除資料-----
+function removeLocalTodos(todo) {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
 
-// add.onClick = () => {
-//   let getLocalStorage = localStorage.getItem('new todo')
+  // 找到第零個的文字
+  // console.log(todo.children[0].innerText)
+  const todoIndex = todo.children[0].innerText;
+  // 找到某文字在第幾個
+  // console.log(todos.indexOf('ddd'))
+  // todos.indexOf(todoIndex) 指你想移除第幾個，後面的 1 是指你想移除幾個
+  todos.splice(todos.indexOf(todoIndex), 1);
 
-//   if (getLocalStorage == null) {
-//     listArray = []
-//   } else {
-//     listArray = JSON.parse(getLocalStorage)
-//   }
-//   listArray.push(input.value)
-//   localStorage.setItem('new todo', JSON.stringify('listArray'))
-// }
+  // 以上事情的簡化版，要從一個陣列中移除東西
+  // const todos = ['amy', 'betty', 'cindy']
+  // console.log(todos.indexOf('amy')) //找出 amy 在陣列中是第幾個，會印出 0
+  // const amyIndex = todos.indexOf('amy')
+  // todos.splice(amyIndex, 1) // 從陣列中刪除 amyIndex，而且只刪 1 個
+  // console.log(todos) // 會印出 ['betty', 'cindy']
 
-// check.addEventListener('click', function () {
-
-// })
+  // 再存回 localstorage，讓 localstorage 裡的資料也刪除
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
