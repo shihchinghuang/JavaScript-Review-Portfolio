ee0f10e11ea57251d9804da4e97d0ddc39642dec
shihchinghuang <82016503+shihchinghuang@users.noreply.github.com>
2021-08-28 11:49:00 +0800

    one-page-features-collection 完成 Nav RWD，刪除無關內容，將表單驗證新增至另一頁面（form-validation）

---
+++ public/form-validation/app.js
@@ -0,0 +1,139 @@
+// ------------------生日下拉選單------------------
+const year = document.querySelector("#year");
+const month = document.querySelector("#month");
+const date = document.querySelector("#date");
+const sendDate = document.querySelector("#sendDate");
+const message = document.querySelector("#message");
+
+let selectedYear = 0;
+let selectedMonth = 0;
+let selectedDate = 0;
+
+function createOption(min, max) {
+  let options = '<option value="0">- 請選擇 -</option>';
+  for (let i = min; i < max + 1; i++) {
+    options += `<option value="${i}" id="">${i}</option>`;
+  }
+  return options;
+}
+
+// switch (month.value) {
+//   case '2':
+//     date.innerHTML = createOption(1, 28)
+//     break
+// }
+
+year.innerHTML = createOption(1911, 2020);
+month.innerHTML = createOption(1, 12);
+// date.innerHTML = createOption(1, 31)
+
+year.addEventListener("change", function () {
+  selectedYear = year.value;
+});
+
+month.addEventListener("change", function () {
+  selectedMonth = month.value;
+
+  if (selectedYear !== "0" && selectedMonth !== "0") {
+    // 選了年和月之後才能開放選日期
+    date.disabled = false;
+
+    // 要得到某年的某個月有幾天，可以用new Date(y, m, 0).getDate()，例如 2019 年的 9 月就是使用new Date(2019, 9, 0).getDate()
+    const maxDate = new Date(selectedYear, selectedMonth, 0).getDate();
+
+    date.innerHTML = createOption(1, maxDate);
+  }
+});
+
+date.addEventListener("change", function () {
+  selectedDate = date.value;
+});
+
+// 檢查是否滿18歲
+sendDate.addEventListener("click", function () {
+  // 取得目前時間
+  const now = new Date();
+
+  const nowY = now.getFullYear();
+  const nowM = now.getMonth() + 1; //回傳為0-11，所以要+1
+  const nowD = now.getDate();
+
+  // 第一步判斷年份，如果大於18就直接判斷已滿18歲
+  if (nowY - selectedYear > 18) {
+    message.innerHTML = "已滿18歲";
+
+    // 第二步年份如果相等，再判斷月份，如果大於18就直接判斷已滿18歲
+  } else if (nowY - selectedYear === 18 && selectedMonth < nowM) {
+    message.innerHTML = "已滿18歲";
+
+    // 第三部如果年份和月份都相等，再判斷日期，大於等於18再判斷已滿18歲
+  } else if (
+    nowY - selectedYear === 18 &&
+    nowM == selectedMonth &&
+    selectedDate <= nowD
+  ) {
+    message.innerHTML = "已滿18歲";
+
+    // 其餘即為未滿18歲
+  } else {
+    message.innerHTML = "未滿18歲";
+  }
+});
+
+// 以下為一個一個寫，但可以像上面一樣寫成一個函式直接呼叫
+// // 選擇年
+// // 選取到的值
+// // 設定為0是因為後續要傳到資料庫的話就不會有值，如果設為1，可能資料庫的人會不知道為什麼是1，初始預設值的概念。
+// let selectedYear = 0
+
+// // 其他準備被選的值
+// let yearOptions = '<option value="0">- 請選擇（年）-</option>'
+
+// // 產生選項
+// for (let i = 1911; i < 2021; i++) {
+//   // 字串在迴圈裡+=也可以疊加，會變成
+//   // <option value="${1911}" id="">${1911}</option>
+//   // <option value="${1912}" id="">${1912}</option>
+//   // <option value="${1913}" id="">${1913}</option>
+//   // ...一直到
+//   // <option value="${2020}" id="">${2020}</option>
+
+//   yearOptions += `<option value="${i}" id="">${i}</option>`
+// }
+
+// year.innerHTML = yearOptions
+
+// year.addEventListener('change', function () {
+//   selectedYear = year.value
+// })
+
+// // 選擇月
+// let selectedMonth = 0
+
+// let monthOptions = '<option value="0">- 請選擇（月）-</option>'
+
+// for (let i = 1; i < 13; i++) {
+//   monthOptions += `<option value="${i}">${i}</option>`
+// }
+
+// month.innerHTML = monthOptions
+
+// month.addEventListener('change', function () {
+//   selectedMonth = month.value
+// })
+
+// //選擇日
+// let selectedDate = 0
+
+// let dateOptions = '<option value="0">- 請選擇（日）-</option>'
+
+// for (let i = 1; i < 32; i++) {
+//   dateOptions += `<option value="${i}">${i}</option>`
+// }
+
+// date.innerHTML = dateOptions
+
+// date.addEventListener('change', function () {
+//   selectedDate = date.value
+// })
+// ------------------生日下拉選單------------------
--- public/one-page-features-collection/app.js
+++ public/one-page-features-collection/app.js
@@ -143,146 +143,6 @@ send.addEventListener("click", function () {
 });
 // ------------------擲骰子------------------
 
-// ------------------生日下拉選單------------------
-const year = document.querySelector("#year");
-const month = document.querySelector("#month");
-const date = document.querySelector("#date");
-const sendDate = document.querySelector("#sendDate");
-const message = document.querySelector("#message");
-
-let selectedYear = 0;
-let selectedMonth = 0;
-let selectedDate = 0;
-
-function createOption(min, max) {
-  let options = '<option value="0">- 請選擇 -</option>';
-  for (let i = min; i < max + 1; i++) {
-    options += `<option value="${i}" id="">${i}</option>`;
-  }
-  return options;
-}
-
-// switch (month.value) {
-//   case '2':
-//     date.innerHTML = createOption(1, 28)
-//     break
-// }
-
-year.innerHTML = createOption(1911, 2020);
-month.innerHTML = createOption(1, 12);
-// date.innerHTML = createOption(1, 31)
-
-year.addEventListener("change", function () {
-  selectedYear = year.value;
-});
-
-month.addEventListener("change", function () {
-  selectedMonth = month.value;
-
-  if (selectedYear !== "0" && selectedMonth !== "0") {
-    // 選了年和月之後才能開放選日期
-    date.disabled = false;
-
-    // 要得到某年的某個月有幾天，可以用new Date(y, m, 0).getDate()，例如 2019 年的 9 月就是使用new Date(2019, 9, 0).getDate()
-    const maxDate = new Date(selectedYear, selectedMonth, 0).getDate();
-
-    date.innerHTML = createOption(1, maxDate);
-  }
-});
-
-date.addEventListener("change", function () {
-  selectedDate = date.value;
-});
-
-// 檢查是否滿18歲
-sendDate.addEventListener("click", function () {
-  // 取得目前時間
-  const now = new Date();
-
-  const nowY = now.getFullYear();
-  const nowM = now.getMonth() + 1; //回傳為0-11，所以要+1
-  const nowD = now.getDate();
-
-  // 第一步判斷年份，如果大於18就直接判斷已滿18歲
-  if (nowY - selectedYear > 18) {
-    message.innerHTML = "已滿18歲";
-
-    // 第二步年份如果相等，再判斷月份，如果大於18就直接判斷已滿18歲
-  } else if (nowY - selectedYear === 18 && selectedMonth < nowM) {
-    message.innerHTML = "已滿18歲";
-
-    // 第三部如果年份和月份都相等，再判斷日期，大於等於18再判斷已滿18歲
-  } else if (
-    nowY - selectedYear === 18 &&
-    nowM == selectedMonth &&
-    selectedDate <= nowD
-  ) {
-    message.innerHTML = "已滿18歲";
-
-    // 其餘即為未滿18歲
-  } else {
-    message.innerHTML = "未滿18歲";
-  }
-});
-
-// 以下為一個一個寫，但可以像上面一樣寫成一個函式直接呼叫
-// // 選擇年
-// // 選取到的值
-// // 設定為0是因為後續要傳到資料庫的話就不會有值，如果設為1，可能資料庫的人會不知道為什麼是1，初始預設值的概念。
-// let selectedYear = 0
-
-// // 其他準備被選的值
-// let yearOptions = '<option value="0">- 請選擇（年）-</option>'
-
-// // 產生選項
-// for (let i = 1911; i < 2021; i++) {
-//   // 字串在迴圈裡+=也可以疊加，會變成
-//   // <option value="${1911}" id="">${1911}</option>
-//   // <option value="${1912}" id="">${1912}</option>
-//   // <option value="${1913}" id="">${1913}</option>
-//   // ...一直到
-//   // <option value="${2020}" id="">${2020}</option>
-
-//   yearOptions += `<option value="${i}" id="">${i}</option>`
-// }
-
-// year.innerHTML = yearOptions
-
-// year.addEventListener('change', function () {
-//   selectedYear = year.value
-// })
-
-// // 選擇月
-// let selectedMonth = 0
-
-// let monthOptions = '<option value="0">- 請選擇（月）-</option>'
-
-// for (let i = 1; i < 13; i++) {
-//   monthOptions += `<option value="${i}">${i}</option>`
-// }
-
-// month.innerHTML = monthOptions
-
-// month.addEventListener('change', function () {
-//   selectedMonth = month.value
-// })
-
-// //選擇日
-// let selectedDate = 0
-
-// let dateOptions = '<option value="0">- 請選擇（日）-</option>'
-
-// for (let i = 1; i < 32; i++) {
-//   dateOptions += `<option value="${i}">${i}</option>`
-// }
-
-// date.innerHTML = dateOptions
-
-// date.addEventListener('change', function () {
-//   selectedDate = date.value
-// })
-// ------------------生日下拉選單------------------
-
 // ------------------猜拳------------------
 const scissors = document.querySelector("#scissors");
 const rock = document.querySelector("#rock");
@@ -377,76 +237,76 @@ draw.addEventListener("click", function () {
 // ------------------抽卡------------------
 
 // ------------------日曆------------------
-const yearAndMoth = document.querySelector("#yearAndMoth");
-const title = document.querySelector("#title");
-const data = document.querySelector("#data");
+// const yearAndMoth = document.querySelector("#yearAndMoth");
+// const title = document.querySelector("#title");
+// const data = document.querySelector("#data");
 
-// new Date() 為產生物件的本地日期與時間
-const now = new Date();
+// // new Date() 為產生物件的本地日期與時間
+// const now = new Date();
 
-// 要得到今天的西元年使用Date物件的getFullYear()，要得到月份使用getMonth()(注意回傳為 0~11)
-const nowY = now.getFullYear();
-const nowM = now.getMonth() + 1; // 回傳為0-11，所以要+1
+// // 要得到今天的西元年使用Date物件的getFullYear()，要得到月份使用getMonth()(注意回傳為 0~11)
+// const nowY = now.getFullYear();
+// const nowM = now.getMonth() + 1; // 回傳為0-11，所以要+1
 
-yearAndMoth.innerHTML = `${nowY} 年 ${nowM} 月`;
+// yearAndMoth.innerHTML = `${nowY} 年 ${nowM} 月`;
 
-const weekList = [
-  "星期日",
-  "星期一",
-  "星期二",
-  "星期三",
-  "星期四",
-  "星期五",
-  "星期六",
-];
+// const weekList = [
+//   "星期日",
+//   "星期一",
+//   "星期二",
+//   "星期三",
+//   "星期四",
+//   "星期五",
+//   "星期六",
+// ];
 
-// for 迴圈的寫法
-let weekDayDisplay = "";
+// // for 迴圈的寫法
+// let weekDayDisplay = "";
 
-for (let i = 0; i < weekList.length; i++) {
-  weekDayDisplay += `<th class="border border-dark"> ${weekList[i]} </th>`;
-}
-title.innerHTML = `<tr class="border border-dark"> ${weekDayDisplay} </tr>`;
+// for (let i = 0; i < weekList.length; i++) {
+//   weekDayDisplay += `<th class="border border-dark"> ${weekList[i]} </th>`;
+// }
+// title.innerHTML = `<tr class="border border-dark"> ${weekDayDisplay} </tr>`;
 
-// map + 箭頭函式的寫法（箭頭函式不用return）
-const weekListMap = weekList
-  .map((v) => `<th class="border border-dark"> ${v} </th>`)
-  .join("");
-title.innerHTML = `<tr class="border border-dark"> ${weekListMap} </tr>`;
+// // map + 箭頭函式的寫法（箭頭函式不用return）
+// const weekListMap = weekList
+//   .map((v) => `<th class="border border-dark"> ${v} </th>`)
+//   .join("");
+// title.innerHTML = `<tr class="border border-dark"> ${weekListMap} </tr>`;
 
-// 要得到某年的某個月有幾天，可以用new Date(y, m, 0).getDate()，例如 2009 年的 9 月就是使用new Date(2009, 9, 0).getDate()
-// 注意，是指上個月的最後一天是幾號
-const days = new Date(nowY, nowM, 0).getDate();
+// // 要得到某年的某個月有幾天，可以用new Date(y, m, 0).getDate()，例如 2009 年的 9 月就是使用new Date(2009, 9, 0).getDate()
+// // 注意，是指上個月的最後一天是幾號
+// const days = new Date(nowY, nowM, 0).getDate();
 
-// 這個月的第一天是星期幾，getDay() 括號中寫日期，可以得到 0-6 的值，代表是星期幾，其中 0 代表星期日。最後一個 1 代表1 號。
-const firstDay = new Date(nowY, nowM - 1, 1).getDay();
+// // 這個月的第一天是星期幾，getDay() 括號中寫日期，可以得到 0-6 的值，代表是星期幾，其中 0 代表星期日。最後一個 1 代表1 號。
+// const firstDay = new Date(nowY, nowM - 1, 1).getDay();
 
-// 本月所有日期先用空陣列
-const daysDataArray = [];
+// // 本月所有日期先用空陣列
+// const daysDataArray = [];
 
-// 補前面的空白
-for (let i = 0; i < firstDay; i++) {
-  daysDataArray.push("");
-}
+// // 補前面的空白
+// for (let i = 0; i < firstDay; i++) {
+//   daysDataArray.push("");
+// }
 
-// 加入本月所有的日期資料，用 1~31 補，如果沒有 i+1 ，會變成 0~30
-for (let i = 0; i < days; i++) {
-  daysDataArray.push(i + 1);
-}
+// // 加入本月所有的日期資料，用 1~31 補，如果沒有 i+1 ，會變成 0~30
+// for (let i = 0; i < days; i++) {
+//   daysDataArray.push(i + 1);
+// }
 
-// 呈現
-let daysDisplay = "";
+// // 呈現
+// let daysDisplay = "";
 
-for (let i = 0; i < daysDataArray.length; i++) {
-  daysDisplay += `<td class="border border-dark">${daysDataArray[i]}</td>`;
+// for (let i = 0; i < daysDataArray.length; i++) {
+//   daysDisplay += `<td class="border border-dark">${daysDataArray[i]}</td>`;
 
-  // 每七個換一行
-  if ((i + 1) % 7 === 0) {
-    daysDisplay += `</tr><tr>`;
-  }
-}
+//   // 每七個換一行
+//   if ((i + 1) % 7 === 0) {
+//     daysDisplay += `</tr><tr>`;
+//   }
+// }
 
-data.innerHTML = `<tr class="border border-dark">${daysDisplay}</tr>`;
+// data.innerHTML = `<tr class="border border-dark">${daysDisplay}</tr>`;
 
 // ------------------日曆------------------
 
@@ -493,62 +353,62 @@ for (let i = 0; i < menuItems.length; i++) {
 // ------------------csv 資料以表格呈現------------------
 // 學生資料 row data：
 // 只有重音符號可以換行
-const rawData = `"學生姓名","國文","數學","英文"
-"陳小花",90,65,77
-"張大頭",80,75,60
-"李一百",100,60,85
-"黃中花",90,65,77
-"王中頭",80,75,60
-"李兩百",100,60,85`;
+// const rawData = `"學生姓名","國文","數學","英文"
+// "陳小花",90,65,77
+// "張大頭",80,75,60
+// "李一百",100,60,85
+// "黃中花",90,65,77
+// "王中頭",80,75,60
+// "李兩百",100,60,85`;
 
-const dataTable = document.querySelector("#dataTable");
+// const dataTable = document.querySelector("#dataTable");
 
-// 把raw data變成陣列，用\n換行分割，一行為一個值
-const rawDataArray = rawData.split("\n");
-// 現在rawDataArray[0]="學生姓名","國文","數學","英文"
-// 現在rawDataArray[1]="陳小花",90,65,77
-// 現在rawDataArray[2]=""張大頭",80,75,60
-// 現在rawDataArray[3]=""李一百",100,60,85
+// // 把raw data變成陣列，用\n換行分割，一行為一個值
+// const rawDataArray = rawData.split("\n");
+// // 現在rawDataArray[0]="學生姓名","國文","數學","英文"
+// // 現在rawDataArray[1]="陳小花",90,65,77
+// // 現在rawDataArray[2]=""張大頭",80,75,60
+// // 現在rawDataArray[3]=""李一百",100,60,85
 
-// 把 rawDataArray[0]再用,分割變成陣列，一項為一個值
-const headingsArray = rawDataArray[0].replaceAll('"', "").split(",");
-// headings[0]="學生姓名"
-// headings[1]="國文"
-// headings[2]="數學"
-// headings[3]="英文"
+// // 把 rawDataArray[0]再用,分割變成陣列，一項為一個值
+// const headingsArray = rawDataArray[0].replaceAll('"', "").split(",");
+// // headings[0]="學生姓名"
+// // headings[1]="國文"
+// // headings[2]="數學"
+// // headings[3]="英文"
 
-// 處理表頭的呈現，和日曆大同小異
-let headingsDisplay = "";
+// // 處理表頭的呈現，和日曆大同小異
+// let headingsDisplay = "";
 
-for (let i = 0; i < headingsArray.length; i++) {
-  headingsDisplay += `<th class="border border-dark"> ${headingsArray[i]} </th>`;
-}
+// for (let i = 0; i < headingsArray.length; i++) {
+//   headingsDisplay += `<th class="border border-dark"> ${headingsArray[i]} </th>`;
+// }
 
-dataTable.innerHTML = `<thead>
-<tr> ${headingsDisplay} </tr>
-</thead>`;
+// dataTable.innerHTML = `<thead>
+// <tr> ${headingsDisplay} </tr>
+// </thead>`;
 
-// 處理表格row的呈現
-let bodyDisplay = "";
-// 從1開始是因為0是表頭
-for (let i = 1; i < rawDataArray.length; i++) {
-  const rows = rawDataArray[i].replaceAll('"', "").split(",");
-  // rows = ["陳小花", "90", "65", "77"]
-  // rows = ["張大頭", "80", "75", "60"]
-  // rows = ["李一百", "100", "60", "85"]
+// // 處理表格row的呈現
+// let bodyDisplay = "";
+// // 從1開始是因為0是表頭
+// for (let i = 1; i < rawDataArray.length; i++) {
+//   const rows = rawDataArray[i].replaceAll('"', "").split(",");
+//   // rows = ["陳小花", "90", "65", "77"]
+//   // rows = ["張大頭", "80", "75", "60"]
+//   // rows = ["李一百", "100", "60", "85"]
 
-  bodyDisplay += `<tr>`;
+//   bodyDisplay += `<tr>`;
 
-  for (let j = 0; j < rows.length; j++) {
-    bodyDisplay += `<td class="border border-dark"> ${rows[j]} </td>`;
-  }
+//   for (let j = 0; j < rows.length; j++) {
+//     bodyDisplay += `<td class="border border-dark"> ${rows[j]} </td>`;
+//   }
 
-  bodyDisplay += `</tr>`;
-}
+//   bodyDisplay += `</tr>`;
+// }
 
-dataTable.innerHTML += `<tbody>
- ${bodyDisplay}
-</tbody>`;
+// dataTable.innerHTML += `<tbody>
+//  ${bodyDisplay}
+// </tbody>`;
 
 // ------------------csv 資料以表格呈現------------------
 
